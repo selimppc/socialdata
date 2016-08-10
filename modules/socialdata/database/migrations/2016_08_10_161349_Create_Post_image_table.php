58370678bc82e931b2f1e4bd6d680602a8feaba2
Shawon <adamilleriam@gmail.com>
2016-08-10 16:21:38 +0600

    migratgion for post_image

---
+++ modules/socialdata/database/migrations/2016_08_10_161349_Create_Post_image_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('post_image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('post');
+            $table->text('url')->nullable();
+            $table->enum('type',['low','thumbnail','standard']);
+            $table->integer('created_by', false, 11);
+            $table->integer('updated_by', false, 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post_image');
+    }
+}
